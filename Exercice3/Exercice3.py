337373ecc88632b1f1384d8c80e8f3c3cb3bb948
laurenzotadjio1999-wq <laurenzotadjio1999@gmail.com>
2026-01-15 17:34:39 -0500

    #Fonction pour trouver le plus petit entier et le plus grand

--- Exercice3/Exercice3.py
+++ Exercice3/Exercice3.py
@@ -7,3 +7,19 @@ class Outils:
         for i in range(10):
             n = int(input("Entrez un nombre : "))
             self.liste.append(n)
+
+#Fonction pour trouver le plus petit entier
+    def min(self):
+        petit = self.liste[0]
+        for x in self.liste:
+            if x < petit:
+                petit = x
+        return petit
+
+# Fonction pour trouver le plus grand entier
+    def max(self):
+        grand = self.liste[0]
+        for x in self.liste:
+            if x > grand:
+                grand = x
+        return grand
