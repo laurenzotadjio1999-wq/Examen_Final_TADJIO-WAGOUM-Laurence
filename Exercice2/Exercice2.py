f19275cc7e406df2581ee4544989a367e8e04442
laurenzotadjio1999-wq <laurenzotadjio1999@gmail.com>
2026-01-15 16:34:06 -0500

    #Création des champs pour recuperer la valeur saisie par l'utilisateur et pour afficher le résultat.

--- Exercice2/Exercice2.py
+++ Exercice2/Exercice2.py
@@ -7,3 +7,18 @@ fen = QWidget()
 # grid layout
 grid = QGridLayout()
 fen.setLayout(grid)
+
+
+# lbl1
+lbl_input1 = QLabel("Entrer la valeur de N:")
+grid.addWidget(lbl_input1, 0, 0)
+# input1
+input1 = QLineEdit(fen)
+grid.addWidget(input1, 0, 1)
+
+# lbl2
+lbl_input2 = QLabel("Voici le Double: ")
+grid.addWidget(lbl_input2, 1, 0)
+# input2
+input2 = QLineEdit(fen)
+grid.addWidget(input2, 1, 1)
