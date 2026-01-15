4525de3779a9328575cd5cb88d046052f80971c7
laurenzotadjio1999-wq <laurenzotadjio1999@gmail.com>
2026-01-15 16:38:01 -0500

    #Création des boutons "Valider l'opération", "Sauvegarder le résultat" et "Charger le résultat". et aussi le label "Valider" qui est l'endroit d'affichage du résultat

--- Exercice2/Exercice2.py
+++ Exercice2/Exercice2.py
@@ -22,3 +22,24 @@ grid.addWidget(lbl_input2, 1, 0)
 # input2
 input2 = QLineEdit(fen)
 grid.addWidget(input2, 1, 1)
+
+# lbl_Valider_opération
+lbl_valider = QLabel(fen)
+grid.addWidget(lbl_valider, 1, 1)
+
+# btn_Valider l'opération
+btn_calculer = QPushButton("Valider l'opération")
+btn_calculer.clicked.connect()
+grid.addWidget(btn_calculer, 3, 1)
+
+
+#bouton2
+btn_save = QPushButton(fen)
+btn_save.setText("Sauvegarder le résultat")
+grid.addWidget(btn_save, 4, 0)
+#btn_save.clicked.connect()
+#bouton3
+btn3_calcul = QPushButton(fen)
+btn3_calcul.setText("Charger résultat")
+grid.addWidget(btn3_calcul, 4, 4)
+#btn3_calcul.clicked.connect()
