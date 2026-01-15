33d5bb53628421a6a045d06bd0f63da931610883
laurenzotadjio1999-wq <laurenzotadjio1999@gmail.com>
2026-01-15 17:32:40 -0500

    #fonction pour saisir les 10 entiers

--- Exercice3/Exercice3.py
+++ Exercice3/Exercice3.py
@@ -2,3 +2,8 @@
 class Outils:
     def __init__(self):
         self.nombres = []
+#fonction pour saisir les 10 entiers
+    def saisir(self):
+        for i in range(10):
+            n = int(input("Entrez un nombre : "))
+            self.liste.append(n)
