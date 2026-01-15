2176e1b330c469e7e5f3f4bfba42d153b6c5926b
laurenzotadjio1999-wq <laurenzotadjio1999@gmail.com>
2026-01-15 15:21:59 -0500

    #Création des lignes d'étoiles: logique de création de nos 02 triangles.

--- Exercice1/Exercice1.py
+++ Exercice1/Exercice1.py
@@ -3,4 +3,9 @@ class Triangle:
     def __init__(self,n):
         self.n = n
         self.symbol = '*'
+#Création des lignes d'étoiles
+    def lignes(self, i):
+        ligne = self.symbol * i
+        espace = " " * (self.n - i)
+        return espace + ligne + " " + ligne
 
